2f01c0cf52303e1bf3f4afaf40c7d965b0df18a6
Yash Bansal <128992188+bansal-yash@users.noreply.github.com>
2024-12-01 10:24:58 +0000

    Assignment 1 upload

--- Assignment7/part2_final/api_hugchat.py
+++ Assignment7/part2_final/api_hugchat.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 
     print(paragraphs)
 
-    sign = Login("bansalyash54321@gmail.com", "Yash88@bansal")
+    sign = Login("", "")
     cookies = sign.login()
 
     cookie_path_dir = "./cookies_snapshot"
